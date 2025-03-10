50c3f4e5d91cce9e2181dcfa9b8a1cc544276324
kamothi <rhkddlf7911@naver.com>
2025-03-10 16:00:40 +0900

    Init

---
+++ src/main/java/org/idiotsdalcheon/emblemeleven/EmblemElevenApplication.java
@@ -0,0 +1,13 @@
+package org.idiotsdalcheon.emblemeleven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmblemElevenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmblemElevenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/idiotsdalcheon/emblemeleven/EmblemElevenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.idiotsdalcheon.emblemeleven;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmblemElevenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
