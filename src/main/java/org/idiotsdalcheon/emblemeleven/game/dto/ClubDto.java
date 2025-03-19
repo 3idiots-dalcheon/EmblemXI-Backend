27ccd7348404a5161b4ab3d20c6b798fb6ba717d
kamothi <rhkddlf7911@naver.com>
2025-03-19 22:30:14 +0900

    feat: random api를 위한 dto 개발

---
+++ src/main/java/org/idiotsdalcheon/emblemeleven/game/dto/ClubDto.java
@@ -0,0 +1,12 @@
+package org.idiotsdalcheon.emblemeleven.game.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@AllArgsConstructor
+@Getter
+public class ClubDto {
+    String clubName;
+    String EmblemURL;
+}
