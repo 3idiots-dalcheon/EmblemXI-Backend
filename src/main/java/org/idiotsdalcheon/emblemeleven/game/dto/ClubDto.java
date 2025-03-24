987871d8f0a98628995f4a2c1cea0f886ac42c11
kamothi <rhkddlf7911@naver.com>
2025-03-24 19:40:20 +0900

    feat: 플레이어 생성 비즈니스 로직 추가

---
+++ src/main/java/org/idiotsdalcheon/emblemeleven/game/dto/ClubDto.java
@@ -0,0 +1,12 @@
+package org.idiotsdalcheon.emblemeleven.game.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ClubDto {
+    private String clubName;
+    private String emblemUrl;
+    private int order;
+}
